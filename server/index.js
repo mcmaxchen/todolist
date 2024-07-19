79e0dc8fe942d6f34c8644936a209881766f36f2
Max <chenmax02@outlook.com>
2024-07-19 11:56:52 +0200

    (Init): index.js and first routes

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+// Utilisation de CORS pour permettre la communication à l'aide de localhost
+app.use(cors());
+
+// Uitilisation de URLEncoded et JSON pour gérer les Request Object des requêtes de type POST
+app.use(express.urlencoded());
+app.use(express.json());
+
+// Routes
+app.post("/", (req, res) => {
+  return res.send({ result: "OK" });
+});
+
+app.listen(8000, () => console.log("Server listening on port 8000"));
