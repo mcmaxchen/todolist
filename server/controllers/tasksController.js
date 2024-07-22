b191aedce5f8ddf8654baa52dfc20dd6102e342a
Max <max.chen@epitech.eu>
2024-07-22 09:27:25 +0200

    (Add): Update task

--- server/controllers/tasksController.js
+++ server/controllers/tasksController.js
@@ -13,15 +13,15 @@ exports.getTasks = async (req, res) => {
 
 exports.getTaskById = async (req, res) => {
   const db = await connection();
-  const { id } = req.body;
+  const { id } = req.params;
 
-  const [task] = await db.query("SELECT * FROM task WHERE id = ?", [id]);
+  const [task] = await db.query("SELECT * FROM tasks WHERE id = ?", [id]);
 
   if (!task) {
     return res.status(404).send({ message: "This task does not exist" });
   }
 
-  res.status(200).send(task);
+  res.status(200).send(task[0]);
 };
 
 exports.createTask = async (req, res) => {
@@ -40,4 +40,39 @@ exports.createTask = async (req, res) => {
   res
     .status(200)
     .send({ id: insert.insertId, message: "Task created successfully" });
+};
+
+exports.updateTask = async (req, res) => {
+  const db = await connection();
+
+  const { id } = req.params;
+  const { title, description, duedate, status } = req.body;
+
+  const [update] = await db.query(
+    "UPDATE tasks SET title = (?), description = (?), duedate = (?), status = (?) WHERE id = (?)",
+    [title, description, duedate, status, id]
+  );
+
+  if (!update) {
+    return res
+      .status(400)
+      .send({ message: "An error occurred while updating the task" });
+  }
+
+  res.status(200).send({ message: "Task updated" });
+};
+
+exports.deleteTask = async (req, res) => {
+  const db = await connection();
+  const { id } = req.params;
+
+  const [remove] = db.query("DELETE FROM tasks WHERE id = (?)", [id]);
+
+  if (!remove) {
+    return res
+      .status(401)
+      .send({ message: "An error occurred while deleting the task" });
+  }
+
+  res.status(200).send({ message: "Task deleted" });
 };
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,9 +1,18 @@
-const { getTasks, createTask } = require("../controllers/tasksController");
+const {
+  getTasks,
+  createTask,
+  getTaskById,
+  updateTask,
+} = require("../controllers/tasksController");
 
 const express = require("express");
 const router = express.Router();
 
 router.get("/tasks", getTasks);
+router.get("/tasks/:id", getTaskById);
+
 router.post("/tasks", createTask);
+
+router.put("/tasks/:id", updateTask);
 
 module.exports = router;
